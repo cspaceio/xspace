b539dcc961b30240aada52495a5b7e68cc219d70
cSpace - IO <36430069+myungkimedia@users.noreply.github.com>
2018-02-21 14:27:23 +0100

    Update - Welcome Page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,15 +79,14 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Welcome To xSpace
+                    Myung Ki
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://laracasts.com">APPS</a>
+                    <a href="https://laravel-news.com">DEV</a>
+                    <a href="https://forge.laravel.com">WEB</a>
+                    <a href="https://github.com/cspaceio/repositories">GitHub</a>
                 </div>
             </div>
         </div>
