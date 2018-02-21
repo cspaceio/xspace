84fd8c14aca0a237590940879bd52f362e2c130e
cSpace - IO <36430069+myungkimedia@users.noreply.github.com>
2018-02-21 14:54:09 +0100

    21/2/18 - Sub Page

---
+++ resources/views/pages/start.blade.php
@@ -0,0 +1,17 @@
+@extends('site')
+
+
+
+@section('header')
+
+	
+
+@endsection
+
+
+
+@section('main')
+
+	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus nobis sit veritatis cumque! Placeat voluptate aut pariatur! Nihil molestiae quos explicabo dignissimos quam cupiditate sequi corporis eaque corrupti. Vel est consequatur cum voluptas atque, quasi sit autem labore natus. Facilis repudiandae nam suscipit unde saepe explicabo officia consequatur nesciunt, nobis quaerat odio, ipsam rem necessitatibus, ea voluptates. Veniam temporibus quos, eos sint pariatur explicabo at amet laboriosam impedit nobis eaque, quas, deserunt magnam eius quia nesciunt! Rerum labore veniam optio ea, non modi, molestiae porro ab tempore illo, temporibus autem quam eius voluptatem nostrum repellendus voluptas nihil atque officiis accusamus incidunt? Provident odit quo asperiores illum quaerat, eum nulla eius dolorum soluta adipisci distinctio ea eligendi cum fugit porro voluptas!
+
+@endsection
