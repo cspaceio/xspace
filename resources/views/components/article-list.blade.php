280132d9f0a1a9ff1157a7406c9fa446d2e595d2
cSpace - IO <36430069+myungkimedia@users.noreply.github.com>
2018-02-23 10:28:06 +0100

    Update 23/2/18

---
+++ resources/views/components/article-list.blade.php
@@ -0,0 +1,14 @@
+<ul>
+	<li>
+		<h4 class="is-size-4">Article Title</h4>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus culpa esse enim possimus consequatur. Libero error labore, aliquam culpa quos impedit nulla esse quae.</p>
+	</li>
+	<li>
+		<h4 class="is-size-4">Article Title</h4>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus culpa esse enim possimus consequatur. Libero error labore, aliquam culpa quos impedit nulla esse quae.</p>
+	</li>
+	<li>
+		<h4 class="is-size-4">Article Title</h4>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus culpa esse enim possimus consequatur. Libero error labore, aliquam culpa quos impedit nulla esse quae.</p>
+	</li>
+</ul>
--- resources/views/pages/start.blade.php
+++ resources/views/pages/start.blade.php
@@ -15,6 +15,7 @@
 
 
 	<section class="sectionOne">
+
 		<div class="columns is-gapless">
 
 
@@ -44,18 +45,21 @@
 
 
 		</div>
+
 	</section>
 
 
 
 	<section class="sectionTwo">
+
 		<div class="columns">
 
 
 
 			<div class="column is-8">
-				
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est ipsa odit ullam, velit at consequatur, quas temporibus ea nesciunt suscipit ipsam molestiae ab ut officia atque quasi, hic alias eos nostrum quia placeat repellat. Est delectus maiores, error sapiente consequatur possimus, inventore laborum, itaque ex voluptatum illo, dolore. Odio impedit ad, nesciunt recusandae, delectus earum ipsum perspiciatis ducimus rerum distinctio, sunt aspernatur architecto explicabo! Quis quaerat nihil aliquam obcaecati excepturi modi atque, dolore distinctio magnam illum, officia voluptatem necessitatibus ex porro repellendus praesentium alias soluta odio at eligendi nesciunt, perspiciatis corporis ratione? Aspernatur ipsum debitis ad quaerat libero, vel, natus mollitia, nemo facere quis suscipit quas. Deserunt molestiae, a! Perspiciatis quaerat iure voluptatem consequatur tempora accusamus eius quae quia laborum.
+
+				<h4 class="is-size-4">Welcom Page</h4>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae sapiente pariatur, nostrum consequuntur natus assumenda hic alias, doloribus iure, aut quisquam labore accusamus aliquam quasi commodi quas dignissimos, placeat sit! Eligendi iste facere nihil excepturi ipsa aut laboriosam explicabo itaque ullam maxime alias aliquid illo saepe error minima fugiat, ducimus consequatur, impedit iusto quisquam. Facere ea, sed cumque ullam, minima inventore deleniti nulla iste iusto, at animi, rerum obcaecati. Tenetur, debitis, necessitatibus voluptatibus ullam, quos, quaerat aliquam a eos dolorum unde cumque! Suscipit perferendis quas non est ex reprehenderit quis, impedit quam labore aperiam atque, vero ullam in ducimus cum natus? Facilis voluptas ducimus enim unde dignissimos nulla. Assumenda est, recusandae error voluptatem ipsa officia natus quidem autem quibusdam minima.</p>
 
 			</div>
 
@@ -63,13 +67,50 @@
 
 			<div class="column is-4">
 				
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, dolorem. Veniam maxime voluptate fugit itaque placeat aut quibusdam ad, sapiente tenetur, ea, dolores doloribus officiis vel unde perspiciatis nihil fugiat reprehenderit, facilis hic! Dolorem consectetur optio dolorum nostrum odit, maxime fugit qui. 
+				" Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, dolorem. Veniam maxime voluptate fugit itaque placeat aut quibusdam ad, sapiente tenetur, ea, dolores doloribus officiis vel unde perspiciatis nihil fugiat reprehenderit, facilis hic! Dolorem consectetur optio dolorum nostrum odit, maxime fugit qui. "" 
 
 			</div>
 
 
 
 		</div>
+
+	</section>
+
+
+
+	<section class="sectionTwo">
+
+		<div class="columns">
+
+
+
+			<div class="column is-4">
+				
+				@component('components.article-list')@endcomponent
+
+			</div>
+
+
+
+			<div class="column is-4">
+				
+				@component('components.posts-list')@endcomponent
+				
+			</div>
+
+
+
+			<div class="column is-4">
+				
+				@component('components.news-list')@endcomponent
+				
+			</div>
+
+
+
+		</div>
+
 	</section>
 
 
