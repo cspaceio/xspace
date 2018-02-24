17ad76b50c5a40170265eddc1b448a5c913847fc
cSpace - IO <36430069+myungkimedia@users.noreply.github.com>
2018-02-24 14:33:49 +0100

    240218 - Article Component Changed

--- resources/views/components/article-list.blade.php
+++ resources/views/components/article-list.blade.php
@@ -1,6 +1,6 @@
 <ul>
 	<li>
-		<h4 class="is-size-4">Article Title</h4>
+		<h4 class="is-size-4">New Article Title</h4>
 		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus culpa esse enim possimus consequatur. Libero error labore, aliquam culpa quos impedit nulla esse quae.</p>
 	</li>
 	<li>
