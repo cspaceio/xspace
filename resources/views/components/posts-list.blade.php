6f000940ceaefbb31be2410493f23d54733a2afd
cSpace - IO <36430069+myungkimedia@users.noreply.github.com>
2018-02-24 15:00:46 +0100

    240218 - component Updates

--- resources/views/components/posts-list.blade.php
+++ resources/views/components/posts-list.blade.php
@@ -1,14 +1,14 @@
 <ul>
 	<li>
-		<h4 class="is-size-4">Blog Post Title</h4>
+		<h4 class="is-size-4">Blog Post Title 1</h4>
 		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus culpa esse enim possimus consequatur. Libero error labore, aliquam culpa quos impedit nulla esse quae.</p>
 	</li>
 	<li>
-		<h4 class="is-size-4">Blog Post Title</h4>
+		<h4 class="is-size-4">Blog Post Title 2</h4>
 		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus culpa esse enim possimus consequatur. Libero error labore, aliquam culpa quos impedit nulla esse quae.</p>
 	</li>
 	<li>
-		<h4 class="is-size-4">Blog Post Title</h4>
+		<h4 class="is-size-4">Blog Post Title 3</h4>
 		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus culpa esse enim possimus consequatur. Libero error labore, aliquam culpa quos impedit nulla esse quae.</p>
 	</li>
 </ul>
