9ed97255f9b1c4ecaf67ea64c3d4d48b62bd544a
cSpace - IO <36430069+myungkimedia@users.noreply.github.com>
2018-02-24 14:41:48 +0100

    240218 - Article List Changed

--- resources/views/components/article-list.blade.php
+++ resources/views/components/article-list.blade.php
@@ -1,14 +1,14 @@
 <ul>
 	<li>
 		<h4 class="is-size-4">New Article Title 1</h4>
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus culpa esse enim possimus consequatur. Libero error labore, aliquam culpa quos impedit nulla esse quae.</p>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veritatis recusandae molestiae ipsam eligendi reiciendis ab, incidunt, quam non reprehenderit illum, possimus error laborum.</p>
 	</li>
 	<li>
 		<h4 class="is-size-4">New Article Title 2</h4>
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus culpa esse enim possimus consequatur. Libero error labore, aliquam culpa quos impedit nulla esse quae.</p>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veritatis recusandae molestiae ipsam eligendi reiciendis ab, incidunt, quam non reprehenderit illum, possimus error laborum.</p>
 	</li>
 	<li>
 		<h4 class="is-size-4">New Article Title 3</h4>
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus culpa esse enim possimus consequatur. Libero error labore, aliquam culpa quos impedit nulla esse quae.</p>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veritatis recusandae molestiae ipsam eligendi reiciendis ab, incidunt, quam non reprehenderit illum, possimus error laborum.</p>
 	</li>
 </ul>
